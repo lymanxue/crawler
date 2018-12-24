cad1f5f3bcf5efbf9db6aff5bfd923172820b357
lyman <lyman@xue.com>
2018-12-24 21:28:26 +0800

    Added Request Module

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import requests
+
+URL = "https://www.baidu.com"
+
+
+def requestURL():
+   url = requests.get(URL)
+   return url
+
 """
     main func
 """
 def main():
     print("Hello World!")
+    print("status = {0}".format(requestURL().status_code))
 
 
 """
